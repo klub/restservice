7d3714317a61c212183ff5176dbc1a9728604446
Bonamy Klu <bonamyklu@gmail.com>
2017-05-02 23:53:53 +0100

    Using a bootstrap script so we can get a zero exit code once java -jar command has brought up the application.

---
+++ bootstrap-for-ci.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+docker-compose -f docker-compose.test.yml up --build > logfile.log &
+
+tail -f logfile.log | while read LOGLINE
+do
+   echo "${LOGLINE}"
+   [[ "${LOGLINE}" == *"Started RestServiceApplication"* ]] && pkill -P $$ tail && echo "FINISHED STARTING UP THE APPLICATION"
+done
