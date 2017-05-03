23b3ec2677d3e2995e6e63be6c561b51ded9b44f
Bonamy Klu <bonamyklu@gmail.com>
2017-05-03 12:45:00 +0100

    Using different commands to keep process in the background

--- bootstrap-for-ci.sh
+++ bootstrap-for-ci.sh
@@ -3,7 +3,7 @@
 set -e
 
 #docker-compose -f docker-compose.test.yml up --build > logfile.log 2>&1
-docker-compose -f docker-compose.test.yml up --build > logfile.log 2>&1 &
+sudo docker-compose -f docker-compose.test.yml up --build > logfile.log 2>&1 &
 
 tail -f logfile.log | while read LOGLINE
 do
